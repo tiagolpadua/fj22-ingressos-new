2cb329c91a396134cffead78b8cb46b7f254ebbd
Tiago Pádua <tiagolpadua@gmail.com>
2020-01-05 22:23:17 -0300

    ex2.6

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,11 +2,28 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	LocalTime horario;
-	Sala sala;
-	Filme filme;
+	private LocalTime horario;
+	@ManyToOne
+	private Sala sala;
+	@ManyToOne
+	private Filme filme;
+
+	/**
+	 * @deprecated hibernate only
+	 */
+	public Sessao() {
+	}
 
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
