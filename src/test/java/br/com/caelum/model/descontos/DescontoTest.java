ee03d2474da0a3542a699ffe04020eaa8390fc21
Tiago Pádua <tiagolpadua@gmail.com>
2020-01-09 16:39:15 -0300

    ex3.5

---
+++ src/test/java/br/com/caelum/model/descontos/DescontoTest.java
@@ -0,0 +1,26 @@
+package br.com.caelum.model.descontos;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.descontos.SemDesconto;
+
+public class DescontoTest {
+	@Test
+	public void naoDeveConcederDescontoParaIngressoNormal() {
+		Sala sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
+		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		BigDecimal precoEsperado = new BigDecimal("32.50");
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+	}
+}
