3f183afd384d6a15770a99945f88ee4a94a732e7
Tiago Pádua <tiagolpadua@gmail.com>
2020-01-09 17:44:39 -0300

    ex4.4

--- src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/caelum/ingresso/controller/FilmeController.java
@@ -18,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.DetalhesDoFilme;
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.rest.OmdbClient;
 
 /**
  * Created by nando on 03/03/17.
@@ -32,6 +34,9 @@ public class FilmeController {
 
 	@Autowired
 	private SessaoDao sessaoDao;
+
+	@Autowired
+	private OmdbClient client;
 
 	@GetMapping({ "/admin/filme", "/admin/filme/{id}" })
 	public ModelAndView form(@PathVariable("id") Optional<Integer> id, Filme filme) {
@@ -91,7 +96,9 @@ public class FilmeController {
 		ModelAndView modelAndView = new ModelAndView("/filme/detalhe");
 		Filme filme = filmeDao.findOne(id);
 		List<Sessao> sessoes = sessaoDao.buscaSessoesDoFilme(filme);
+		Optional<DetalhesDoFilme> detalhesDoFilme = client.request(filme);
 		modelAndView.addObject("sessoes", sessoes);
+		modelAndView.addObject("detalhes", detalhesDoFilme.orElse(new DetalhesDoFilme()));
 		return modelAndView;
 	}
 }
