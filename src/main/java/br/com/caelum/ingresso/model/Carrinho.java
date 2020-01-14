4be36a1ea709ba905a9c1429bbe3b97ad3decab3
Tiago Pádua <tiagolpadua@gmail.com>
2020-01-14 08:57:35 -0300

    ex6.6

--- src/main/java/br/com/caelum/ingresso/model/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/model/Carrinho.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,5 +26,9 @@ public class Carrinho {
 
 	public boolean isSelecionado(Lugar lugar) {
 		return ingressos.stream().map(Ingresso::getLugar).anyMatch(lugarDoIngresso -> lugarDoIngresso.equals(lugar));
+	}
+
+	public BigDecimal getTotal() {
+		return ingressos.stream().map(Ingresso::getPreco).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
 	}
 }
