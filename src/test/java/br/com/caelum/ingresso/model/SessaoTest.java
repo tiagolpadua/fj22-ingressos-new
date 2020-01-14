f6460f5d795c5f84d5ea8ee0b1cbf99ac413b33f
Tiago Pádua <tiagolpadua@gmail.com>
2020-01-14 08:39:09 -0300

    ex5.5

--- src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
@@ -3,6 +3,9 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,5 +18,21 @@ public class SessaoTest {
 		BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
 		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
 		Assert.assertEquals(somaDosPrecosDaSalaEFilme, sessao.getPreco());
+	}
+
+	@Test
+	public void garanteQueOLugarA1EstaOcupadoEOsLugaresA2EA3Disponiveis() {
+		Lugar a1 = new Lugar("A", 1);
+		Lugar a2 = new Lugar("A", 2);
+		Lugar a3 = new Lugar("A", 3);
+		Filme rogueOne = new Filme("Rogue One", Duration.ofMinutes(120), "SCI_FI", new BigDecimal("12.0"));
+		Sala eldorado7 = new Sala("Eldorado 7", new BigDecimal("8.5"));
+		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), rogueOne, eldorado7);
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO, a1);
+		Set<Ingresso> ingressos = Stream.of(ingresso).collect(Collectors.toSet());
+		sessao.setIngressos(ingressos);
+		Assert.assertFalse(sessao.isDisponivel(a1));
+		Assert.assertTrue(sessao.isDisponivel(a2));
+		Assert.assertTrue(sessao.isDisponivel(a3));
 	}
 }
